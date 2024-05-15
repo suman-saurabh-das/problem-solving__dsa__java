0d679f0c799b89d2722fb7b8cb4b6d9db3b43a44
Suman Saurabh Das <dsumansaurabh@gmail.com>
2024-05-15 20:07:51 +0530

    String - 008 Split a string in balanced strings

--- README.md
+++ README.md
@@ -64,6 +64,7 @@
 - 005 [Find words containing character](https://github.com/suman-saurabh-das/problem-solving__dsa__java/blob/main/src/string/leetcode/P005_Find_words_containing_characters.java)
 - 006 [Goal parser interpretation](https://github.com/suman-saurabh-das/problem-solving__dsa__java/blob/main/src/string/leetcode/P006_Goal_parser_interpretation.java)
 - 007 [Maximum number of words found in sentences](https://github.com/suman-saurabh-das/problem-solving__dsa__java/blob/main/src/string/leetcode/P007_Maximum_number_of_words_found_in_sentences.java)
+- 008 [Split a string in balanced strings](https://github.com/suman-saurabh-das/problem-solving__dsa__java/blob/main/src/string/leetcode/P008_Split_a_string_balanced_strings.java)
 
 </details>
 
---
+++ src/string/leetcode/P008_Split_a_string_balanced_strings.java
@@ -0,0 +1,42 @@
+package string.leetcode;
+
+/*
+    1221. Split a string in balanced strings
+    https://leetcode.com/problems/split-a-string-in-balanced-strings/description/
+
+    Input: s = "RLRRLLRLRL"
+    Output: 4
+    Input: s = "RLRRRLLRLL"
+    Output: 2
+    Input: s = "LLLLRRRR"
+    Output: 1
+*/
+
+public class P008_Split_a_string_balanced_strings {
+    public static void main(String[] args) {
+        String s1 = "RLRRLLRLRL", s2 = "RLRRRLLRLL", s3 = "LLLLRRRR";
+        System.out.println("Maximum number of balanced strings : " + balancedStringSplit(s1));
+        System.out.println("Maximum number of balanced strings : " + balancedStringSplit(s2));
+        System.out.println("Maximum number of balanced strings : " + balancedStringSplit(s3));
+    }
+
+    // SOLUTION USING LOOP - time complexity : O(n)
+    public static int balancedStringSplit(String s) {
+        // Loop through the characters and find,
+        // how many balanced pairs of R-L can be formed.
+        int countR = 0, countL = 0, numBalancedStrings = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == 'R') {
+                countR++;
+            } else {
+                countL++;
+            }
+            if (countR == countL) {
+                numBalancedStrings++;
+                countR = 0;
+                countL = 0;
+            }
+        }
+        return numBalancedStrings;
+    }
+}
